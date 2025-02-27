3ba2016683fd78a0a6f2ec30d5334e0ce2c88193
Shekhar Singh <shekharsingh745681@gmail.com>
2025-02-27 20:52:28 +0530

    add

--- ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9a5041eb85ee82af65e20d106f58a23d9a484de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08988303ae79e8e0527b187fc575677d607fe40e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
